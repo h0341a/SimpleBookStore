9258f23df8d2124f5a2189be838e4559511b4724
h0341a <qianaiwuyu@gmail.com>
2019-07-04 18:10:35 +0800

    整合了mybatis

--- src/main/java/study/ssm/dao/impl/UserDAOImpl.java
+++ src/main/java/study/ssm/dao/impl/UserDAOImpl.java
@@ -1,29 +1,30 @@
 package study.ssm.dao.impl;
 
+import org.apache.ibatis.session.SqlSession;
 import study.ssm.dao.UserDAO;
 import study.ssm.entity.User;
 import study.ssm.utils.ConnMariaDB;
+import study.ssm.utils.MySqlSessionFactory;
 
+import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDAOImpl implements UserDAO {
+
     @Override
     public User queryByUsername(String username) {
-        String sql = "SELECT * FROM user Where username = ?";
-        ConnMariaDB.connInit();
         try {
-            PreparedStatement ps = ConnMariaDB.connection.prepareStatement(sql);
-            ps.setObject(1, username);
-            ResultSet rs = ps.executeQuery();
-            if(rs.next()){
-                return new User(rs.getString("username"), rs.getString("password"));
-            }
-        } catch (SQLException e) {
-            System.err.println("获取sql语句执行对象失败!");
+            MySqlSessionFactory.getSqlSession();
+            UserDAO userDAO = MySqlSessionFactory.session.getMapper(UserDAO.class);
+            User user = userDAO.queryByUsername(username);
+            MySqlSessionFactory.closeSession();
+            return user;
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
         return null;
     }
 }
---
+++ src/main/java/study/ssm/utils/MySqlSessionFactory.java
@@ -0,0 +1,26 @@
+package study.ssm.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MySqlSessionFactory {
+
+    public static SqlSession session;
+
+    public static void getSqlSession() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        session = sqlSessionFactory.openSession();
+    }
+
+    public static void closeSession(){
+        session.close();
+    }
+
+}
